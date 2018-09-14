8be25af69edc91e57077254af47717d4d933a474
LinearLaw <17817363770@163.com>
2018-09-14 15:58:45 +0800

    add webpack compress type

---
+++ webpack_type/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+ 
+module.exports = {
+    entry:{
+        index: './src/index.js',
+        index2: './src/index2.js'
+    }, 
+    output:{
+        path:path.resolve(__dirname,'dist'),
+        //filename前面我们可以使用一个变量[name],这个就表示获取entry里面的key作为文件名加在前面
+        //打出来是index-bundle.js
+        //和index2-bundle.js
+        filename:'[name]-bundle.js' 
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            chunks:['index'], //添加引入的js,也就是entry中的key
+            filename:'index.html',
+            minify:{
+                collapseWhitespace:true //折叠空白区域 也就是压缩代码
+            },
+            hash:true,
+            title:'I love China',
+            template: './src/index.html' //模板地址
+        }),
+        new HtmlWebpackPlugin({
+            chunks:['index2'], //添加引入的js,也就是entry中的key
+            filename:'index2.html',
+            minify:{
+                collapseWhitespace:true //折叠空白区域 也就是压缩代码
+            },
+            hash:true,
+            title:'第二个页面',
+            template: './src/index2.html' //模板地址
+        })
+    ]
